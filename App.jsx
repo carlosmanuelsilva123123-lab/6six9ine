908fbe8d8555e5a107a81a39fd2a950bfb60cc82
carlosmanuelsilva123123-lab <carlosmanuelsilva123123@gmail.com>
2026-04-20 22:13:57 -0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,5 +1,12 @@
-import { useEffect, useState } from "react";
-import MiniKit from "@worldcoin/minikit-js";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 export default function App() {
   const [wallet, setWallet] = useState(null);
