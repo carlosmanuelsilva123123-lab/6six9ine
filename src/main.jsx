10781c6d0c64265a51bae9596eb002d8f0d9cce2
carlosmanuelsilva123123-lab <carlosmanuelsilva123123@gmail.com>
2026-04-21 19:40:55 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { MiniKit } from '@worldcoin/minikit-js'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+// 🔥 Inicializar MiniKit
+MiniKit.install()
+
+ReactDOM.createRoot(document.getElementById('app')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
